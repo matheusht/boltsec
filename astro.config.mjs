cc64d5bc8502c9c1af8497ac6796878c34d909e2
matheusht <dev.matheustheodoro@gmail.com>
2025-04-23 15:14:00 -0300

    services card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import {i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration"
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
 });
